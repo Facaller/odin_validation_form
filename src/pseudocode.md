9c2e6fabcfea319abac923de0aa9866cfba33ad8
Facaller <ashwin.metal13@gmail.com>
2025-05-29 12:17:02 +0200

    Complete basic structure. Add validation method skeleton

--- src/pseudocode.md
+++ src/pseudocode.md
@@ -1,3 +1,7 @@
 1 - create form and elements in html
 
 2 - add simple css to help visualize
+
+3 - create object for form validators
+
+4 - create object for form elements
---
+++ src/validator.js
@@ -0,0 +1,33 @@
+export class Validator {
+    constructor () {
+
+    }
+
+    groupValidation () {
+
+    }
+
+    nameValidation () {
+
+    }
+
+    emailValidation () {
+
+    }
+
+    countryValidation () {
+
+    }
+
+    postalValidation () {
+
+    }
+
+    passValidation () {
+
+    }
+
+    passConfirmationValidation () {
+
+    }
+}
