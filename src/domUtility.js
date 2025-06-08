16ada94c2ba4dec3477a5adacce78b78514f14ab
Facaller <ashwin.metal13@gmail.com>
2025-06-08 13:55:14 +0200

    Add DOM elements to DOM module, add span elements to html

--- src/domUtility.js
+++ src/domUtility.js
@@ -2,12 +2,33 @@ import { Validator } from "./validator";
 
 export class Elements {
     constructor () {
-        this.formContainer   = document.querySelector('.form-container')
-        this.name            = document.querySelector('#name')
-        this.email           = document.querySelector('#email')
-        this.country         = document.querySelector('#country')
-        this.postal          = document.querySelector('#postal-code')
-        this.password        = document.querySelector('#password')
-        this.confirmPassword = document.querySelector('#pass-confirmation')
+        this.form          = document.querySelector('.form-container');
+        this.nameInput     = document.querySelector('#name');
+        this.emailInput    = document.querySelector('#email');
+        this.countryInput  = document.querySelector('#country');
+        this.postalInput   = document.querySelector('#postal-code');
+        this.passwordInput = document.querySelector('#password');
+        this.confirmInput  = document.querySelector('#pass-confirmation');
+
+        this.nameError     = document.querySelector('#name-error');
+        this.emailError    = document.querySelector('#email-error');
+        this.countryError  = document.querySelector('#country-error');
+        this.postalError   = document.querySelector('#postal-error');
+        this.passwordError = document.querySelector('#password-error');
+        this.confirmError  = document.querySelector('#confirm-error');
+    }
+}
+
+export class DOMHandler {
+    constructor () {
+        this.elements = new Elements();
+    }
+
+    checkFormValidation = () => {
+        const form = this.elements.form;
+
+        form.addEventListener('input', () => {
+
+        })
     }
 }
